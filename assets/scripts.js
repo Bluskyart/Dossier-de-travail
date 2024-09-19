34324fb6aa1c5bed0b3719482fb2f75d501824bc
Guerwan Lescrohart <glescrohart@gmail.com>
2024-09-19 17:13:44 +0200

    fix: return J-Query

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,38 +1,15 @@
-export class Utils {
-    constructor(selector) {
-        this.elements = Utils.getSelector(selector);
-        this.element = this.get(0);
-        return this;
-    }
-
-    static getSelector(selector, context) {
-        if (selector && typeof selector !== 'string') {
-            if (selector.length !== undefined) {
-                return selector;
-            }
-            return [selector];
-        }
-        context = context || document;
-
-        // For performance reasons, use getElementById
-        // eslint-disable-next-line no-control-regex
-        const idRegex = /^#(?:[\w-]|\\.|[^\x00-\xa0])*$/;
-        if (idRegex.test(selector)) {
-            const el = document.getElementById(selector.substring(1));
-            return el ? [el] : [];
-        }
-        return [].slice.call(context.querySelectorAll(selector) || []);
-    }
-    get(index) {
-        if (index !== undefined) {
-            return this.elements[index];
-        }
-        return this.elements;
-    }
-}
-
-Utils.eventListeners = {};
-
-export default function $utils(selector) {
-    return new Utils(selector);
-}
+$(document).ready(function() {
+    $('.gallery').mauGallery({
+        columns: {
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 3
+        },
+        lightBox: true,
+        lightboxId: 'myAwesomeLightbox',
+        showTags: true,
+        tagsPosition: 'top'
+    });
+});
