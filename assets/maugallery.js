85a789f9edd6070104361103a828c326be13d45f
Guerwan Lescrohart <glescrohart@gmail.com>
2024-09-19 22:35:36 +0200

    fix: J-query to Javascript + delete bootstrap and adapt JS

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -1,140 +1,185 @@
-export class Utils {
-  constructor(selector) {
-      this.elements = Utils.getSelector(selector);
-      this.element = this.get(0);
-      return this;
+function mauGallery(options) {
+  var defaults = {
+      columns: 3,
+      lightBox: true,
+      lightboxId: null,
+      showTags: true,
+      tagsPosition: 'bottom',
+      navigation: true
+  };
+
+  options = Object.assign(defaults, options);
+  var tagsCollection = [];
+  
+  var gallery = document.querySelector('.gallery');
+  createRowWrapper(gallery);
+
+  if (options.lightBox) {
+      createLightBox(gallery, options.lightboxId, options.navigation);
   }
 
-  each(func) {
-      if (!this.elements.length) {
-          return this;
+  initListeners(options);
+
+  // Parcourir chaque élément de la galerie
+  var galleryItems = gallery.querySelectorAll('.gallery-item');
+  galleryItems.forEach(function(item) {
+      responsiveImageItem(item);
+      moveItemInRowWrapper(item);
+      wrapItemInColumn(item, options.columns);
+      var theTag = item.getAttribute('data-gallery-tag');
+      if (options.showTags && theTag && tagsCollection.indexOf(theTag) === -1) {
+          tagsCollection.push(theTag);
       }
-      this.elements.forEach((el, index) => {
-          func.call(el, el, index);
-      });
-      return this;
-  }
-  children() {
-      return new Utils(this.element.children);
-  }
-  data(name, value) {
-      return this.attr('data-' + name, value);
-  }
-  on(events, listener) {
-      events.split(' ').forEach((eventName) => {
-          this.each((el) => {
-              const tNEventName = Utils.setEventName(el, eventName);
-              if (!Array.isArray(Utils.eventListeners[tNEventName])) {
-                  Utils.eventListeners[tNEventName] = [];
-              }
-              Utils.eventListeners[tNEventName].push(listener);
+  });
 
-              // https://github.com/microsoft/TypeScript/issues/28357
-              if (el) {
-                  el.addEventListener(eventName.split('.')[0], listener);
-              }
-          });
-      });
-
-      return this;
+  if (options.showTags) {
+      showItemTags(gallery, options.tagsPosition, tagsCollection);
   }
-  attr(name, value) {
-      if (value === undefined) {
-          if (!this.element) {
-              return '';
+
+  gallery.style.display = 'block';
+}
+
+function initListeners(options) {
+  document.querySelectorAll('.gallery-item').forEach(function(item) {
+      item.addEventListener('click', function() {
+          if (options.lightBox && item.tagName === 'IMG') {
+              openLightBox(item, options.lightboxId);
           }
-          return this.element.getAttribute(name);
-      }
-      this.each((el) => {
-          el.setAttribute(name, value);
       });
-      return this;
-  }
-  find(selector) {
-      return new Utils(Utils.getSelector(selector, this.element));
-  }
-  hasClass(className) {
-      if (!this.element) {
-          return false;
-      }
-      return this.element.classList.contains(className);
-  }
-  removeClass(classNames) {
-      this.each((el) => {
-          // IE doesn't support multiple arguments
-          classNames.split(' ').forEach((className) => {
-              el.classList.remove(className);
-          });
-      });
-      return this;
-  }
-  addClass(classNames = '') {
-      this.each((el) => {
-          // IE doesn't support multiple arguments
-          classNames.split(' ').forEach((className) => {
-              el.classList.add(className);
-          });
-      });
-      return this;
-  }
-  hide() {
-      this.each((el) => {
-          el.style.display = 'none';
-      });
-  }
-  show() {
-      this.each((el) => {
-          el.style.display = '';
-      });
-  }
-  static getSelector(selector, context) {
-      if (selector && typeof selector !== 'string') {
-          if (selector.length !== undefined) {
-              return selector;
-          }
-          return [selector];
-      }
-      context = context || document;
+  });
 
-      // For performance reasons, use getElementById
-      // eslint-disable-next-line no-control-regex
-      const idRegex = /^#(?:[\w-]|\\.|[^\x00-\xa0])*$/;
-      if (idRegex.test(selector)) {
-          const el = document.getElementById(selector.substring(1));
-          return el ? [el] : [];
-      }
-      return [].slice.call(context.querySelectorAll(selector) || []);
-  }
-  get(index) {
-      if (index !== undefined) {
-          return this.elements[index];
-      }
-      return this.elements;
-  }
-  static setEventName(el, eventName) {
-      // Need to verify https://stackoverflow.com/questions/1915341/whats-wrong-with-adding-properties-to-dom-element-objects
-      const elementUUId = el.eventEmitterUUID;
-      const uuid = elementUUId || Utils.generateUUID();
-      // eslint-disable-next-line no-param-reassign
-      el.eventEmitterUUID = uuid;
-      return Utils.getEventName(eventName, uuid);
-  }
-  static generateUUID() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-          // eslint-disable-next-line no-bitwise
-          const r = (Math.random() * 16) | 0;
-          // eslint-disable-next-line no-bitwise
-          const v = c === 'x' ? r : (r & 0x3) | 0x8;
-          return v.toString(16);
+  document.querySelectorAll('.nav-link').forEach(function(link) {
+      link.addEventListener('click', filterByTag);
+  });
+
+  var prevBtn = document.querySelector('.mg-prev');
+  var nextBtn = document.querySelector('.mg-next');
+
+  if (prevBtn) {
+      prevBtn.addEventListener('click', function() {
+          prevImage(options.lightboxId);
       });
   }
-  static getEventName(eventName, uuid) {
-      return eventName + '__EVENT_EMITTER__' + uuid;
+
+  if (nextBtn) {
+      nextBtn.addEventListener('click', function() {
+          nextImage(options.lightboxId);
+      });
   }
 }
 
-Utils.eventListeners = {};
+function createRowWrapper(element) {
+  if (!element.querySelector('.gallery-items-row')) {
+      var rowWrapper = document.createElement('div');
+      rowWrapper.classList.add('gallery-items-row', 'row');
+      element.appendChild(rowWrapper);
+  }
+}
 
-export default function $utils(selector) {
-  return new Utils(selector);
+function wrapItemInColumn(element, columns) {
+  var columnClass = '';
+  if (typeof columns === 'number') {
+      columnClass = `col-${Math.ceil(12 / columns)}`;
+  } else if (typeof columns === 'object') {
+      columnClass = `col-${Math.ceil(12 / columns.xs)} col-sm-${Math.ceil(12 / columns.sm)} col-md-${Math.ceil(12 / columns.md)} col-lg-${Math.ceil(12 / columns.lg)} col-xl-${Math.ceil(12 / columns.xl)}`;
+  }
+
+  var columnWrapper = document.createElement('div');
+  columnWrapper.classList.add('item-column', 'mb-4');
+  columnWrapper.classList.add(...columnClass.split(' '));
+  element.parentNode.insertBefore(columnWrapper, element);
+  columnWrapper.appendChild(element);
+}
+
+function moveItemInRowWrapper(element) {
+  var rowWrapper = document.querySelector('.gallery-items-row');
+  rowWrapper.appendChild(element);
+}
+
+function responsiveImageItem(element) {
+  if (element.tagName === 'IMG') {
+      element.classList.add('img-fluid');
+  }
+}
+
+function createLightBox(gallery, lightboxId, navigation) {
+  var modal = document.createElement('div');
+  modal.classList.add('modal', 'fade');
+  modal.id = lightboxId ? lightboxId : 'galleryLightbox';
+  modal.setAttribute('tabindex', '-1');
+  modal.setAttribute('role', 'dialog');
+  modal.setAttribute('aria-hidden', 'true');
+
+  modal.innerHTML = `
+      <div class="modal-dialog" role="document">
+          <div class="modal-content">
+              <div class="modal-body">
+                  ${navigation ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;">&lt;</div>' : ''}
+                  <img class="lightboxImage img-fluid" alt="Image content"/>
+                  ${navigation ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;">&gt;</div>' : ''}
+              </div>
+          </div>
+      </div>`;
+  
+  gallery.appendChild(modal);
+}
+
+function openLightBox(element, lightboxId) {
+  var lightbox = document.getElementById(lightboxId);
+  var lightboxImage = lightbox.querySelector('.lightboxImage');
+  lightboxImage.src = element.src;
+  $(lightbox).modal('toggle');
+}
+
+function prevImage() {
+  navigateImage('prev');
+}
+
+function nextImage() {
+  navigateImage('next');
+}
+
+function navigateImage(direction) {
+  var activeImage = document.querySelector('.lightboxImage').src;
+  var images = document.querySelectorAll('.gallery-item');
+  var imagesArray = Array.prototype.slice.call(images);
+  var currentIndex = imagesArray.findIndex(img => img.src === activeImage);
+
+  if (direction === 'prev') {
+      currentIndex = currentIndex === 0 ? imagesArray.length - 1 : currentIndex - 1;
+  } else {
+      currentIndex = currentIndex === imagesArray.length - 1 ? 0 : currentIndex + 1;
+  }
+
+  document.querySelector('.lightboxImage').src = imagesArray[currentIndex].src;
+}
+
+function showItemTags(gallery, position, tags) {
+  var tagList = '<li class="nav-item"><span class="nav-link active active-tag" data-images-toggle="all">Tous</span></li>';
+  tags.forEach(function(tag) {
+      tagList += `<li class="nav-item"><span class="nav-link" data-images-toggle="${tag}">${tag}</span></li>`;
+  });
+
+  var tagBar = document.createElement('ul');
+  tagBar.classList.add('my-4', 'tags-bar', 'nav', 'nav-pills');
+  tagBar.innerHTML = tagList;
+
+  if (position === 'top') {
+      gallery.insertBefore(tagBar, gallery.firstChild);
+  } else {
+      gallery.appendChild(tagBar);
+  }
+}
+
+function filterByTag() {
+  var tag = this.getAttribute('data-images-toggle');
+  document.querySelectorAll('.gallery-item').forEach(function(item) {
+      var parent = item.closest('.item-column');
+      parent.style.display = tag === 'all' || item.getAttribute('data-gallery-tag') === tag ? 'block' : 'none';
+  });
+
+  document.querySelectorAll('.active-tag').forEach(function(tagElement) {
+      tagElement.classList.remove('active', 'active-tag');
+  });
+  this.classList.add('active', 'active-tag');
 }
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,54 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
+// Carousel functionality
+document.addEventListener('DOMContentLoaded', function () {
+    const carouselInner = document.querySelector('.carousel-inner');
+    const items = document.querySelectorAll('.carousel-item');
+    let currentIndex = 0;
+
+    function updateCarousel() {
+        carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    document.querySelector('.carousel-control-next').addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % items.length;
+        updateCarousel();
     });
+
+    document.querySelector('.carousel-control-prev').addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + items.length) % items.length;
+        updateCarousel();
+    });
+
+    // Initialize carousel
+    updateCarousel();
+});
+
+// Gallery functionality
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryItems = document.querySelectorAll('.gallery-item');
+    
+    // Optionally, you could create tags dynamically or add more advanced filtering logic here
+    const tags = Array.from(new Set(Array.from(galleryItems).map(item => item.dataset.galleryTag)));
+
+    // Create a filter menu
+    const filterMenu = document.createElement('div');
+    filterMenu.className = 'gallery-filter-menu';
+    tags.forEach(tag => {
+        const button = document.createElement('button');
+        button.textContent = tag;
+        button.addEventListener('click', () => {
+            filterGallery(tag);
+        });
+        filterMenu.appendChild(button);
+    });
+    document.getElementById('gallery').insertBefore(filterMenu, document.querySelector('.gallery'));
+
+    function filterGallery(selectedTag) {
+        galleryItems.forEach(item => {
+            if (item.dataset.galleryTag === selectedTag || selectedTag === 'All') {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    }
 });
